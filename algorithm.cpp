69908db16b50efe8ff7422ac650b71b1998d7e03
DarlanGabriel <darlan.acari@gmail.com>
2020-03-07 10:55:04 -0300

    Analise de conversao

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <string.h>
+#include <cstdlib>
 using namespace std;
 
 int main(){
-    char c;
-    cin >> c;
-    printf("%x \n", c);
+    char palavra[4], palcon[7];
+    scanf("%s", palavra);
+    for(int i = 0; i < sizeof(palavra) - 1; i++){
+        sprintf(palcon + i * 2, "%02x", palavra[i]);
+    }
+    printf("%s \n", palcon);
     return 0;
 }
