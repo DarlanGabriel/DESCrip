6e722998d01d33ae8cc5c9a8be58addc9a5067cf
DarlanGabriel <darlan.acari@gmail.com>
2020-03-07 11:09:02 -0300

    Pequeno Avanço

--- algorithm.cpp
+++ algorithm.cpp
@@ -3,11 +3,18 @@
 #include <cstdlib>
 using namespace std;
 
+void conv(char c, char *b){
+    sprintf(b, "%02x", c);
+}
+
 int main(){
     char palavra[4], palcon[7];
+    char b[2];
     scanf("%s", palavra);
     for(int i = 0; i < sizeof(palavra) - 1; i++){
-        sprintf(palcon + i * 2, "%02x", palavra[i]);
+        conv(palavra[i], b);
+        palcon[i * 2] = b[0];
+        palcon[i * 2 + 1] = b[1];
     }
     printf("%s \n", palcon);
     return 0;
