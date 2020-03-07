f18bf1ce4c7e368c9b91d2e62ec4b32d56039c3e
DarlanGabriel <darlan.acari@gmail.com>
2020-03-07 15:23:29 -0300

    Desconversor finalizado

--- algorithm.cpp
+++ algorithm.cpp
@@ -4,7 +4,6 @@
 #include <cstddef>
 #include <type_traits>
 using namespace std;
-string palcon;
 
 void convHex(string palavra, string &palcon, char * b){
     for(int i = 0; i < palavra.length(); i++){
@@ -14,16 +13,27 @@ void convHex(string palavra, string &palcon, char * b){
     }
 }
 
+void desHex(char * c, string &paldes, char * b){
+    while(c[0] != 0){
+        sscanf(c, "%2x", b);
+        paldes.push_back(b[0]);
+        c = c + 2;
+    }
+}
+
 int main(){
-    char b[2];
+    char b[2], c[65];
     int i = 0;
-    string palavra, palcon;
+    string palavra, palcon, paldes;
     getline(cin, palavra);
     convHex(palavra, palcon, b);
     while(palcon[i] != 0 && !(palcon[i] < 33)){
         cout << palcon[i];
+        c[i] = palcon[i];
         i++;
     }
     cout << "\n";
+    desHex(c, paldes, b);
+    cout << paldes << "\n";
     return 0;
 }
