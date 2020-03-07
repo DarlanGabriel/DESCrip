3c1591b81e49f5b3e695afef6c5902db823504bc
DarlanGabriel <darlan.acari@gmail.com>
2020-03-07 16:22:04 -0300

    Adicionado converter bin

--- algorithm.cpp
+++ algorithm.cpp
@@ -21,10 +21,99 @@ void desHex(char * c, string &paldes, char * b){
     }
 }
 
+void convBin(string palcon, string &palbin){
+    int i = 0;
+    while(palcon[i] != 0){
+        if(palcon[i] == '0'){
+            palbin.push_back('0');
+            palbin.push_back('0');
+            palbin.push_back('0');
+            palbin.push_back('0');
+        }else if(palcon[i] == '1'){
+            palbin.push_back('0');
+            palbin.push_back('0');
+            palbin.push_back('0');
+            palbin.push_back('1');
+        }else if(palcon[i] == '2'){
+            palbin.push_back('0');
+            palbin.push_back('0');
+            palbin.push_back('1');
+            palbin.push_back('0');
+        }else if(palcon[i] == '3'){
+            palbin.push_back('0');
+            palbin.push_back('0');
+            palbin.push_back('1');
+            palbin.push_back('1');
+        }else if(palcon[i] == '4'){
+            palbin.push_back('0');
+            palbin.push_back('1');
+            palbin.push_back('0');
+            palbin.push_back('0');
+        }else if(palcon[i] == '5'){
+            palbin.push_back('0');
+            palbin.push_back('1');
+            palbin.push_back('0');
+            palbin.push_back('1');
+        }else if(palcon[i] == '6'){
+            palbin.push_back('0');
+            palbin.push_back('1');
+            palbin.push_back('1');
+            palbin.push_back('0');
+        }else if(palcon[i] == '7'){
+            palbin.push_back('0');
+            palbin.push_back('1');
+            palbin.push_back('1');
+            palbin.push_back('1');
+        }else if(palcon[i] == '8'){
+            palbin.push_back('1');
+            palbin.push_back('0');
+            palbin.push_back('0');
+            palbin.push_back('0');
+        }else if(palcon[i] == '9'){
+            palbin.push_back('1');
+            palbin.push_back('0');
+            palbin.push_back('0');
+            palbin.push_back('1');
+        }else if(palcon[i] == 'a'){
+            palbin.push_back('1');
+            palbin.push_back('0');
+            palbin.push_back('1');
+            palbin.push_back('0');
+        }else if(palcon[i] == 'b'){
+            palbin.push_back('1');
+            palbin.push_back('0');
+            palbin.push_back('1');
+            palbin.push_back('1');
+        }else if(palcon[i] == 'c'){
+            palbin.push_back('1');
+            palbin.push_back('1');
+            palbin.push_back('0');
+            palbin.push_back('0');
+        }else if(palcon[i] == 'd'){
+            palbin.push_back('1');
+            palbin.push_back('1');
+            palbin.push_back('0');
+            palbin.push_back('1');
+        }else if(palcon[i] == 'e'){
+            palbin.push_back('1');
+            palbin.push_back('1');
+            palbin.push_back('1');
+            palbin.push_back('0');
+        }else{
+            palbin.push_back('1');
+            palbin.push_back('1');
+            palbin.push_back('1');
+            palbin.push_back('1');
+        }
+        i++;
+    }
+}
+
 int main(){
     char b[2], c[65];
     int i = 0;
-    string palavra, palcon, paldes;
+    string palavra, palcon, paldes; //parte hexadecimal
+    string palbin;
     getline(cin, palavra);
     convHex(palavra, palcon, b);
     while(palcon[i] != 0 && !(palcon[i] < 33)){
@@ -35,5 +124,8 @@ int main(){
     cout << "\n";
     desHex(c, paldes, b);
     cout << paldes << "\n";
+
+    convBin(palcon, palbin);
+    cout << palbin << "\n";
     return 0;
 }
